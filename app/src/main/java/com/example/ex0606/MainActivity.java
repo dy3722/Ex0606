40f1613028d347dbf7a16b42ad1ec996b9d2e0e5
dy3722 <dy3722@bs.amalnet.k12.il>
2025-01-26 19:29:59 +0200

    Finishing the design

--- app/src/main/java/com/example/ex0606/MainActivity.java
+++ app/src/main/java/com/example/ex0606/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex0606;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    ImageView iView;
+    Button btn;
+    Random rnd;
+    int num;
+    String st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iView = findViewById(R.id.iView);
+        btn = findViewById(R.id.btn);
+        rnd = new Random();
+    }
+
+    public void go(View view)
+    {
+
     }
 }
